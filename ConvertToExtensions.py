b8bf5236494237e0ba1787613316a13ce88c137e
kgullikson88 <kevin.gullikson@gmail.com>
2014-08-25 11:24:43 -0500

    Divide the errors by the continuum as well as the flux values (error propogation)

--- ConvertToExtensions.py
+++ ConvertToExtensions.py
@@ -1,98 +1,175 @@
-import HelperFunctions
 import sys
 import os
-import numpy
+import FittingUtilities
+
 import matplotlib.pyplot as plt
 from astropy.io import fits
-import DataStructures
-import FittingUtilities
-from astropy import wcs
-from itertools import combinations
-from scipy import optimize
-from scipy.interpolate import InterpolatedUnivariateSpline as spline
-import time
-from collections import defaultdict
+from astropy import time, units as u
+import numpy as np
 
-fitorder = 3
-
-def main():
-  # Read in the logfile to get the original data filenames
-  original_files = defaultdict( lambda : defaultdict( lambda : defaultdict(list) ) )
-  extracted_files = defaultdict( lambda : defaultdict( lambda: defaultdict( lambda : defaultdict(str) ) ) )
-  for band in ["H", "K"]:
-    logfilename = [f for f in os.listdir(band) if "IGRINS_DT_Log"  in f and f.endswith("%s.txt" %band)][0]
-    infile = open("%s/%s" %(band, logfilename))
-    lines = infile.readlines()
-    infile.close()
-
-    for line in lines[2:]:
-      segments = line.split(",")
-      groupnum = int(segments[2])
-      objtype = segments[5]
-      if objtype.lower() == "tar" or objtype.lower() == "std":
-        original_files[groupnum][objtype][band].append("%s/%s" %(band, segments[0]))
-
-        #Find the extracted files
-        ex = [f for f in os.listdir(band) if objtype in f and "G%i" %groupnum in f and f.endswith(".tpn.fits")]
-        for fname in ex:
-          ordernum = int(fname.split(".tr.")[-1].split(".tpn")[0])
-          if ordernum > 1:
-            extracted_files[groupnum][objtype][ordernum][band] = "%s/%s" %(band, fname)
-
-  # Read in the data, and assign a wavelength grid to it
-  for group in extracted_files.keys():
-    for objtype in extracted_files[group].keys():
-      column_list = []
-      header_list = []
-      for ordernum in sorted(extracted_files[group][objtype].keys()):
-        for band in extracted_files[group][objtype][ordernum].keys():
-          fname = extracted_files[group][objtype][ordernum][band]
-          hdulist = fits.open(fname)
-          header = hdulist[0].header
-          order = HelperFunctions.ReadFits(fname)[0]
-
-          #Check for a wave file
-          wavefile = "%s/School/Research/Useful_Datafiles/IGRINS_Datafiles/SDC%s_order%.3i.wavedata" %(os.environ["HOME"], band, ordernum)
-          if os.path.isfile(wavefile):
-            x,y = numpy.loadtxt(wavefile, unpack=True)
-            fit = numpy.poly1d(numpy.polyfit(x,y, fitorder))
-            order.x = fit(order.x)
-          else:
-            print "Wavelength file not found!"
-            a, b = header['CRVAL1'], header['CDELT1']
-            order.x = a + b*order.x
-
-          #Fit the continuum
-          order.cont = FittingUtilities.Continuum(order.x, order.y, lowreject=2, highreject=2, fitorder=5)
-          order.cont[order.cont < 1] = 1.0
-
-          #Prepare output
-          column = {"wavelength": order.x,
-                    "flux": order.y,
-                    "continuum": order.cont,
-                    "error": order.err}
-          hinfo = [["AP-NUM", header["AP-NUM"]],
-                   ["AP-COEF1", header["AP-COEF1"]],
-                   ["AP-COEF2", header["AP-COEF2"]],
-                   ["AP-WIDTH", header["AP-WIDTH"]],
-                   ["BAND", band],
-                   ["FNAME", fname, 'original filename']]
-          column_list.append(column)
-          header_list.append(hinfo)
-
-      outfilename = "%s_G%i.fits" %(objtype, group)
-      print "Outputting to %s" %outfilename
-      sorter = [i[0] for i in sorted(enumerate(column_list), key = lambda c: c[1]["wavelength"][0])]
-      clist = []
-      hlist = []
-      for i in sorter:
-        clist.append(column_list[i])
-        hlist.append(header_list[i])
-      HelperFunctions.OutputFitsFileExtensions(clist, fname, outfilename, mode="new", headers_info=hlist)
+import HelperFunctions
 
 
-  
+def ReadFile(filename, columns):
+    orders = HelperFunctions.ReadFits(filename)
+    header_info = []
+    for i, order in enumerate(orders):
+        # Cut off the edges, where the blaze is really bad
+        order = order[100:-100]
+
+        # Convert to air wavelengths!
+        wave_A = order.x * u.nm.to(u.angstrom)
+        n = 1.0 + 2.735182e-4 + 131.4182 / wave_A ** 2 + 2.76249e8 / wave_A ** 4
+        order.x /= n
+
+        # order.cont = FittingUtilities.Continuum(order.x, order.y, fitorder=9, lowreject=2, highreject=10)
+        #order.cont[order.cont < 10] = 10
+        #order.y /= order.cont/order.cont.mean()
+        #order.cont = order.cont.mean() * np.ones(order.size())
+        order.cont = FittingUtilities.Iterative_SV(order.y.copy(), 351, 3, lowreject=2, highreject=10)
+        plt.plot(order.x, order.y, 'k-', alpha=0.4)
+        plt.plot(order.x, order.cont, 'r-', alpha=0.4)
+
+        order.y /= order.cont / order.cont.mean()
+        order.err /= order.cont / order.cont.mean()
+        order.cont = order.cont.mean() * np.ones(order.size())
+
+        column = {"wavelength": order.x,
+                  "flux": order.y,
+                  "continuum": order.cont,
+                  "error": order.err}
+        columns.append(column)
+    plt.show()
+    return columns
 
 
 if __name__ == "__main__":
-  main()
+    # Read command line arguments
+    for arg in sys.argv[1:]:
+        if "num" in arg:
+            r = arg.partition("=")[-1].split("-")
+            lownum = int(r[0])
+            highnum = int(r[1])
+        else:
+            filename = arg
+
+    print "Converting file {:s}".format(filename)
+    columns = []
+    columns = ReadFile(filename, columns)
+
+    # Get the other band
+    if "SDCH" in filename:
+        filename = filename.replace("SDCH", "SDCK")
+    elif "SDCK" in filename:
+        filename = filename.replace("SDCK", "SDCH")
+    columns = ReadFile(filename, columns)
+
+    # Sort the columns by increasing wavelength
+    columns = sorted(columns, key=lambda c: c['wavelength'][0])
+
+    #plt.show()
+
+    # Prepare fits keywords
+    date_obs = []
+    zenith_angle = []
+    humidity = []
+    temperature = []
+    pressure = []
+    original_fnames = sorted(
+        ["20140708/SDCH_20140708_{:s}.fits".format(str(i).zfill(4)) for i in range(lownum, highnum + 1)])
+    for i, fname in enumerate(original_fnames):
+        print "Reading header info from {:s}".format(fname)
+        #Get the observation time
+        header = fits.getheader(fname)
+        t = header['DATE-OBS']
+        t = "{:s}T{:s}".format(t[:10], t[11:])
+        date = t.partition('T')[0]
+        #print t
+        t_jd = time.Time(t, format='isot', scale='utc').jd
+        if i == len(original_fnames) - 1:
+            exptime = float(header['EXPTIME']) * u.s.to(u.day)
+            t_jd += exptime
+
+            objname = header['OBJECT']
+        date_obs.append(t_jd)
+
+        #Get the ambient pressure, temperature, and humidity
+        #Read in weather information (archived data is downloaded from weather.as.utexas.edu)
+        homedir = os.environ["HOME"]
+        weather_file = homedir + "/School/Research/Useful_Datafiles/Weather.dat"
+        infile = open(weather_file)
+        lines = infile.readlines()
+        infile.close()
+        times = []
+        RH = []
+        P = []
+        T = []
+        idx = 0
+        bestindex = 0
+        difference = 9e9
+        for line in lines[1:]:
+            segments = line.split()
+            if date in segments[0]:
+                segments = segments[1].split(",")
+                t = segments[0]
+                weather_time = time.Time("{:s}T{:s}".format(date, t), format='isot', scale='utc').jd
+                #t_seg = t.split(":")
+                #weather_time = 3600*float(t_seg[0]) + 60*float(t_seg[1]) + float(t_seg[2])
+                if np.abs(t_jd - weather_time) < difference:
+                    difference = np.abs(t_jd - weather_time)
+                    bestindex = idx
+                times.append(segments[0])
+                T.append(float(segments[3]))
+                RH.append(float(segments[4]))
+                P.append(float(segments[5]))
+                idx += 1
+        humidity.append(RH[bestindex])
+        temperature.append(T[bestindex])
+        pressure.append(P[bestindex])
+
+        zenith_angle.append(90.0 - float(header['ALT']))
+
+    #Figure out the average values for each of the quantities
+    ZD = np.mean(zenith_angle)
+    RH = np.mean(humidity)
+    T = np.mean(temperature)
+    P = np.mean(pressure)
+    t_jd = (min(date_obs) + max(date_obs)) / 2.0
+
+
+
+
+    #Output as a fits file with binary tables
+    outfilename = "{:s}.fits".format(objname.replace(" ", "_"))
+    print "Outputting to {:s}".format(outfilename)
+    pri_hdu = fits.PrimaryHDU()
+    pri_hdu.header['OBJECT'] = objname
+    pri_hdu.header['DATE-OBS'] = time.Time(t_jd, format='jd').isot
+    pri_hdu.header['UT'] = pri_hdu.header['DATE-OBS'].split("T")[-1]
+    pri_hdu.header['HUMIDITY'] = RH
+    pri_hdu.header['AIRTEMP'] = T
+    pri_hdu.header['BARPRESS'] = P
+    pri_hdu.header['ZD'] = ZD
+    hdulist = fits.HDUList([pri_hdu, ])
+
+    for i in range(len(columns)):
+        column_dict = columns[i]
+        cols = []
+        for key in column_dict.keys():
+            cols.append(fits.Column(name=key, format="D", array=column_dict[key]))
+        cols = fits.ColDefs(cols)
+        tablehdu = fits.BinTableHDU.from_columns(cols)
+        tablehdu.header.name = "Order {:d}".format(i + 1)
+        hdulist.append(tablehdu)
+
+    hdulist.writeto(outfilename, clobber=True, output_verify='ignore')
+    hdulist.close()
+
+
+
+
+
+
+
+
+
