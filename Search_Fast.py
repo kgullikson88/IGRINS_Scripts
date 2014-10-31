e8de4a9d0eb52e67a3186b48f389588d8f85e332
kgullikson88 <kevin.gullikson@gmail.com>
2014-10-31 14:47:40 -0500

    Temporary changes for IRS48 search

--- Search_Fast.py
+++ Search_Fast.py
@@ -5,9 +5,12 @@ import GenericSearch
 
 # Define regions contaminated by telluric residuals or other defects. We will not use those regions in the cross-correlation
 badregions = [[0, 1510],  # Blue end of H band (lots of water absorption)
-              [1561, 1615],  # CO2 band that is often poorly corrected (for now at least...)
+              #[1561, 1615],  # CO2 band that is often poorly corrected (for now at least...)
               [1740, 2090],  #In between H and K bands (lots of water absorption)
-              [2348, 2500]]  #Red end of K band (lots of water absorption)
+              [2348, 2500],  #Red end of K band (lots of water absorption)
+              [1510, 1520],  #Temporary...
+              [1688,1740],
+              [2313, 2350]]
 
 
 if __name__ == "__main__":
@@ -26,9 +29,16 @@ if __name__ == "__main__":
 
     GenericSearch.CompanionSearch(fileList,
                                   extensions=extensions,
-                                  resolution=40000.0,
+                                  resolution=45000.0,
                                   trimsize=trimsize,
-                                  modeldir='/media/FreeAgent_Drive/SyntheticSpectra/Sorted/Stellar/NearIR/')
+                                  Tvalues=range(2700, 3900, 100),
+                                  metal_values=[-0.5, 0.0, 0.5],
+                                  vsini_values=[1.0,],
+                                  logg_values=[3.5,],
+                                  vbary_correct=False,
+				  debug=False,
+				  badregions=badregions,
+                                  modeldir='/Volumes/DATADRIVE/Stellar_Models/Sorted/Stellar/NearIR/')
 
 
 
