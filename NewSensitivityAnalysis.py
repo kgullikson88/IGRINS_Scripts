76a867a050164ee975ea730ba952442263a062c3
kgullikson88 <kevin.gullikson@gmail.com>
2015-02-21 10:24:16 -0600

    Using new analysis scripts in Sensitivity

--- NewSensitivityAnalysis.py
+++ NewSensitivityAnalysis.py
@@ -5,10 +5,13 @@ import sys
 
 import Sensitivity
 import StarData
-
+import SpectralTypeRelations
 import Search_slow
 
-if __name__ == '__main__':
+MS = SpectralTypeRelations.MainSequence()
+
+
+def check_sensitivity():
     fileList = []
     for arg in sys.argv[1:]:
         if 1:
@@ -27,7 +30,14 @@ if __name__ == '__main__':
                         badregions=badregions, interp_regions=interp_regions,
                         metal_values=(0.0,),
                         vsini_values=(5,),
-                        Tvalues=range(3000, 6100, 100),
+                        Tvalues=range(3000, 6000, 100),
                         debug=False,
                         addmode='simple',
                         output_mode='hdf5')
+
+
+if __name__ == '__main__':
+    if 'analyze' in sys.argv[1]:
+        Sensitivity.analyze_sensitivity(hdf5_file='Sensitivity.hdf5')
+    else:
+        check_sensitivity()
