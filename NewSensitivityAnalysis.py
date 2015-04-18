761928cdeb6067d629c2bf75b120f24457eb59d3
kgullikson88 <kevin.gullikson@gmail.com>
2015-04-18 09:20:53 -0500

    Settings update.

--- NewSensitivityAnalysis.py
+++ NewSensitivityAnalysis.py
@@ -54,7 +54,7 @@ def check_sensitivity():
 if __name__ == '__main__':
     if '--analyze' in sys.argv[1]:
         # Make the 2d plots
-        df = Sensitivity.analyze_sensitivity(hdf5_file='Sensitivity_known.hdf5')
+        df = Sensitivity.analyze_sensitivity(hdf5_file='Sensitivity.hdf5', interactive=False)
 
     elif '--marginalize' in sys.argv[1]:
         fig, ax = Sensitivity.marginalize_sensitivity(infilename='Sensitivity_Dataframe.csv')
