e5039dedb05e685f04c327e5e8d8da9f4f8344f2
kgullikson88 <kevin.gullikson@gmail.com>
2015-06-01 16:22:40 -0500

    Settings change.

--- ccdlist.py
+++ ccdlist.py
@@ -6,4 +6,4 @@ from astropy.io import fits as pyfits
 for fname in sys.argv[1:]:
     header = pyfits.getheader(fname)
     # hdulist = pyfits.open(fname)
-    print fname, header['object'], header['exptime']/60.0
+    print fname, header['object'], header['DATE-OBS'], header['exptime'] / 60.0
