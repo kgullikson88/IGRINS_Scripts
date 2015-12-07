e548a80c5f91b4ed4a2055e3cc181895191aefb7
Kevin Gullikson <kevin.gullikson@gmail.com>
2015-12-07 16:49:22 -0600

    Finer search grid.

--- Search_primary.py
+++ Search_primary.py
@@ -4,9 +4,9 @@ import GenericSearch
 
 # Define regions contaminated by telluric residuals or other defects. We will not use those regions in the cross-correlation
 # badregions = [[475, 495]]
-badregions = []
+badregions = [[1665., 1800.]]
 interp_regions = []
-trimsize = 10
+trimsize = 1
 
 if "darwin" in sys.platform:
     modeldir = "/Volumes/DATADRIVE/Stellar_Models/PHOENIX/Stellar/Vband/"
@@ -31,6 +31,8 @@ if __name__ == '__main__':
     prim_vsini = [None for _ in fileList]
 
     # Use this one for the real data search
+    Tvalues = range(7000, 10000, 200) + range(10000, 30000, 400)
+    Tvalues = range(7000, 10000, 250) + range(10000, 30000, 500)
     GenericSearch.slow_companion_search(fileList, prim_vsini,
                                         hdf5_file=hdf5_filename,
                                         extensions=True,
@@ -40,12 +42,12 @@ if __name__ == '__main__':
                                         badregions=badregions,
                                         metal_values=(0.0),
                                         logg_values=(3.5, 4.0, 4.5,),
-                                        vsini_values=(10, 40, 80, 150, 250),
-                                        Tvalues=range(7000, 20000, 1000),
+                                        vsini_values=range(50, 300, 25),
+                                        Tvalues=Tvalues,
                                         observatory='McDonald',
                                         debug=False,
                                         reject_outliers=False,
                                         vbary_correct=False,
                                         addmode='all',
                                         output_mode='hdf5',
-                                        output_file='CCF_primary.hdf5')
+                                        output_file='CCF_primary_20151129.hdf5')
